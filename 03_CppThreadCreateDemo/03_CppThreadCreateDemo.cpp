ed43550d609101fdbac74202b14070c1c016fb27
钰 刘 <18391128562@163.com>
2026-03-23 14:53:51 +0800

    C++11库提供的线程创建函数 C++11新标准引入了一个新的类std::thread（需要包含头文件<thread>），使用这个类可以将任何签名形式的函数作为线程函数，同时可跨平台

---
+++ 03_CppThreadCreateDemo/03_CppThreadCreateDemo.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <thread>
+
+void threadproc1() {
+    while (true) {
+        printf("I am New thread1");
+    }
+}
+
+void threadproc2(int a, int b) {
+    while (true) {
+        printf("I am New thread2");
+    }
+}
+
+int main() {
+    std::thread t1(&threadproc1);
+    std::thread t2(&threadproc2, 1, 2);
+    while (true) {
+
+    }
+    return 0;
+}
+
