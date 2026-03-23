08bcceeed05445acc782d6e18a5844d46f1b07bd
钰 刘 <18391128562@163.com>
2026-03-23 14:51:58 +0800

    C库函数创建线程 在Windows操作系统上，微软实现的C库也提供了一套用于创建线程的函数（当然这个函数底层还是调用相应的操作系统平台的线程创建 API）。在实际项目开发中推荐使用这个函数来创建线程而不是使用CreateThread函数

---
+++ 02_WinCRT_beginthreadDemo/02_WinCRT_beginthreadDemo.cpp
@@ -0,0 +1,18 @@
+﻿#include <process.h>
+#include <stdio.h>
+
+unsigned int __stdcall threadfun(void* arg) {
+    while (true) {
+        printf("I am New Thread\n");
+    }
+}
+
+int main() {
+    unsigned int threadid;
+    _beginthreadex(0, 0, threadfun, 0, 0, &threadid);
+    while (true) {
+
+    }
+    return 0;
+}
+
