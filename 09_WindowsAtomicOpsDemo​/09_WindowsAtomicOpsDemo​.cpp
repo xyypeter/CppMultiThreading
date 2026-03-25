dbd63fc3ca3298010a4d3d4548947de32146dfcd
钰 刘 <18391128562@163.com>
2026-03-25 09:51:01 +0800

    Windows 平台上整型变量的原子操作

---
+++ 09_WindowsAtomicOpsDemo​/09_WindowsAtomicOpsDemo​.cpp
@@ -0,0 +1,14 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+    LONG nPreValue = 99;
+    LONG nPostValue = InterlockedIncrement(&nPreValue);
+
+    printf("nPreValue=%d, nPostValue=%d\n", nPreValue, nPostValue);
+
+    return 0;
+}
+
+
