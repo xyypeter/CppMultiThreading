225a77a8c6612d375f32099ad486575ae8c6f7d1
钰 刘 <18391128562@163.com>
2026-03-25 10:12:25 +0800

    C++11整型原子变量

---
+++ 10_Cpp11AtomicDemo/10_Cpp11AtomicDemo.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <atomic>
+#include <stdio.h>
+
+int main(){
+    std::atomic<int> value;
+    value = 99;
+    printf("%d\n", (int)value);
+    
+    //自增1，原子操作
+    value++;
+    printf("%d\n", (int)value);
+    return 0;
+}
+
+
