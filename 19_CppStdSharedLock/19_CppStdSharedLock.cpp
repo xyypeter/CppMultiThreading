62c66fabbe8256e7bf742b911ea11b9ce7109f37
钰 刘 <18391128562@163.com>
2026-04-03 10:35:22 +0800

    std::shared_lock 案例 1.使用读多写少的场景 2.写独占访问 3.读多线程同时读

---
+++ 19_CppStdSharedLock/19_CppStdSharedLock.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <thread>
+#include <shared_mutex>
+
+std::shared_mutex rwMutex;
+int sharedData = 0;
+
+// 写线程（独占）
+void writer() {
+    for (int i = 0; i < 5; ++i) {
+        std::unique_lock<std::shared_mutex> lock(rwMutex);
+        sharedData++;
+        std::cout << "write: " << sharedData << std::endl;
+    }
+}
+
+// 读线程（共享）
+void reader(int id) {
+    for (int i = 0; i < 5; ++i) {
+        std::shared_lock<std::shared_mutex> lock(rwMutex);
+        std::cout << "reader " << id << " read: " << sharedData << std::endl;
+    }
+}
+
+int main() {
+    std::thread w(writer);
+    std::thread r1(reader, 1);
+    std::thread r2(reader, 2);
+
+    w.join();
+    r1.join();
+    r2.join();
+}
