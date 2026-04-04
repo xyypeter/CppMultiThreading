1ededdc054ecaac4a4753e0ebe3088d79d98c730
钰 刘 <18391128562@163.com>
2026-04-04 17:51:18 +0800

    C库非线程安全函数

---
+++ 25_C_Lib_Thread_Safety/25_C_Lib_Thread_Safety.c
@@ -0,0 +1,42 @@
+﻿#include <time.h>
+#include <stdio.h>
+
+void test1(){
+    time_t tNow = time(NULL);
+    time_t tEnd = tNow + 1800;
+    //注意下面两行的区别
+    struct tm* ptm = localtime(&tNow);
+    struct tm* ptmEnd = localtime(&tEnd);
+
+    char szTmp[50] = { 0 };
+    strftime(szTmp, 50, "%H:%M:%S", ptm);
+
+    //struct tm* ptmEnd = localtime(&tEnd);
+    char szEnd[50] = { 0 };
+    strftime(szEnd, 50, "%H:%M:%S", ptmEnd);
+    printf("%s \n", szTmp);
+    printf("%s \n", szEnd);
+}
+
+void test2() {
+    time_t tNow = time(NULL);
+    time_t tEnd = tNow + 1800;
+    //注意下面两行的区别
+    struct tm* ptm = localtime(&tNow);
+
+    char szTmp[50] = { 0 };
+    strftime(szTmp, 50, "%H:%M:%S", ptm);
+
+    struct tm* ptmEnd = localtime(&tEnd);
+    char szEnd[50] = { 0 };
+    strftime(szEnd, 50, "%H:%M:%S", ptmEnd);
+    printf("%s \n", szTmp);
+    printf("%s \n", szEnd);
+}
+
+int main(){
+    test1();
+    test2();
+
+    return 0;
+}
